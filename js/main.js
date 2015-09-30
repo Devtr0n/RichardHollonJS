abeeedf8d86aca58d9e158d2801c0e8aaaf92b17
unknown <rhollon@rhollon.tea.state.tx.us>
2015-09-30 15:59:15 -0500

    randomized background image & fixed jQuery bug for MSIE reference

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready( function() {
 
+	/////////////////////////// Background Image Slideshow ////////////////////////////////////////////////
+	var images = ['seacliff.jpg', 'japan.jpg', 'gunkanjima.jpg', 'hashima.jpg'];
+	$('body').css({'background-image': 'url(../src/images/' + images[Math.floor(Math.random() * images.length)] + ')'});	
+
+	//http://www.techerator.com/2010/11/how-to-make-a-css-background-slideshow-with-jquery/ 
+	// var currImg = 1;
+	// var intID = setInterval(changeImg, 9000);
+	 
+	// /* image rotator with animation */
+	// function changeImg(){
+		// $('body').animate({opacity: 0}, 1000, function(){
+			// $('body').css('background-image','url(../src/images/' + images[Math.floor(Math.random() * images.length)] +')');
+		// }).animate({opacity: 1}, 1000);
+	// }	 	
+	/////////////////////////// END - Background Image Slideshow ////////////////////////////////////////////////
+
 	// Logo
 	var $logo 	= $('#logo');
 	 if (location.href.indexOf("#") != -1) {
@@ -7,7 +23,6 @@ $(document).ready( function() {
         	$logo.show();
         }
     }
-    
 	// Show logo 
 	$('#tab-container .tab a').click(function() {
 	  $logo.slideDown('slow');
@@ -16,19 +31,20 @@ $(document).ready( function() {
 	$('#tab-about').click(function() {
 	  $logo.slideUp('slow');
 	});	
-function animMeter(){
-    $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
-}
-animMeter();
+	
+	function animMeter(){
+		$(".meter > span").each(function() {
+					$(this)
+						.data("origWidth", $(this).width())
+						.width(0)
+						.animate({
+							width: $(this).data("origWidth")
+						}, 1200);
+				});
+	}
+	animMeter();
 
-      $('#tab-container').easytabs({
+    $('#tab-container').easytabs({
         animate			: true,
         updateHash		: true,
         transitionIn	: 'slideDown',
@@ -39,4 +55,4 @@ animMeter();
                     animMeter();
             }
         });
-    });
+});
